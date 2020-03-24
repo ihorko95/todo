e12d5c54032de51c74b677df72410db699fd7449
Ihor Kolisnyk <ihorko95@gmail.com>
2020-03-24 11:11:30 +0100

    Updates

---
+++ precache-manifest.cbf81a3413b9de1269e8f65202276d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f0c653857417e67df69eebb526dfb3",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "2fd517d4fb4a2150ce43",
+    "url": "/todo/static/css/main.6a17640b.chunk.css"
+  },
+  {
+    "revision": "59aa1585f19d28baaeb4",
+    "url": "/todo/static/js/2.0ec2e835.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.0ec2e835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd517d4fb4a2150ce43",
+    "url": "/todo/static/js/main.afba820f.chunk.js"
+  },
+  {
+    "revision": "10fd3631c43d335a3db9",
+    "url": "/todo/static/js/runtime-main.2a8ec137.js"
+  }
+]);
